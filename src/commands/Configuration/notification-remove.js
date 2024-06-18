b644c914438e29c8a2c9d71cfe75f2645b511786
Abhigyan Mandal <iamabhigyan5253@gmail.com>
2024-06-18 12:53:49 +0530

    Fixed /notification-remove command

--- src/commands/Configuration/notification-remove.js
+++ src/commands/Configuration/notification-remove.js
@@ -1,55 +1,59 @@
-const {SlashCommandBuilder, PermissionFlagsBits, ChannelType} = require('discord.js');
-const NotificationConfig = require('../../models/NotificationConfig');
+const {
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    ChannelType,
+} = require("discord.js");
+const NotificationConfig = require("../../models/NotificationConfig");
 
 /**
- * 
- * @param {import('commandkit').SlashCommandProps;} param0 
+ *
+ * @param {import('commandkit').SlashCommandProps;} param0
  */
-async function run ({interaction}) {
-    try {
-        await interaction.deferReply({ ephemeral : true });
-        const targetChannelId = interaction.options.getString('youtube-id')
-        const targetNotificationChannel = interaction.options.getChannel('target-channel')
+async function run({ interaction }) {
+    await interaction.deferReply({ ephemeral: true });
+    const targetChannelId = interaction.options.getString("youtube-id");
+    const targetNotificationChannel = interaction.options.getChannel("target-channel");
 
-        const targetChannel = NotificationConfig.findOne({
-            ytChannelId : targetChannelId,
-            notificationChannelId : targetNotificationChannel.id
-        });
-        
-        if (!targetChannel) {
-            interaction.followUp('This channel is not configured for notification');
-            return;
-        }
-        NotificationConfig.findOneAndDelete({ _id: targetChannel._id })
-            .then((e) => {
-                interaction.followUp('Turned off notifications for that channel');
-            })
-            .catch((e) => {
-                interaction.followUp('There was an error in database. Please try again later');
-            })
+    const docs = await NotificationConfig.findOne({
+        ytChannelId: targetChannelId,
+        notificationChannelId: targetNotificationChannel.id,
+    });
 
-    } catch (error) {
-        console.log(`Error in ${__filename}\n`, error);
+    if (!docs) {
+        interaction.followUp("This channel is not configured for notification");
+        return;
     }
+    NotificationConfig.findOneAndDelete({ _id : docs._id })
+        .then((e) => {
+            interaction.followUp("Turned off notifications for that channel");
+        })
+        .catch((e) => {
+            interaction.followUp(
+                "There was an error in database. Please try again later"
+            );
+        });
 }
 
 const data = new SlashCommandBuilder()
-    .setName('notification-remove')
-    .setDescription('Removes the notification configuration')
+    .setName("notification-remove")
+    .setDescription("Removes the notification configuration")
     .setDMPermission(false)
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-    .addStringOption((option) => 
+    .addStringOption((option) =>
         option
-            .setName('youtube-id')
-            .setDescription('The ID of the channel')
+            .setName("youtube-id")
+            .setDescription("The ID of the channel")
             .setRequired(true)
     )
-    .addChannelOption((option) => 
+    .addChannelOption((option) =>
         option
-            .setName('target-channel')
-            .setDescription('The channel you want to remove notification from')
+            .setName("target-channel")
+            .setDescription("The channel you want to remove notification from")
             .setRequired(true)
-            .addChannelTypes(ChannelType.GuildAnnouncement, ChannelType.GuildText)
-    )
+            .addChannelTypes(
+                ChannelType.GuildAnnouncement,
+                ChannelType.GuildText
+            )
+    );
 
 module.exports = { data, run };
